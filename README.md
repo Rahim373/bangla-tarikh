8e45c81a9907cd4d7206ce36ae539f745fd67528
RAHIMPC\Abdur Rahim <rahim.prsf@gmail.com>
2018-08-14 22:42:19 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bangla Tarikh
+
+**bangla-tarikh** is an npm package that gives dates according to [Bengali calendar](https://en.wikipedia.org/wiki/Bengali_calendars).
---
+++ dist/index.d.ts
@@ -0,0 +1,32 @@
+/**
+ * Returns the year of the given date in number format
+ * @param date given date object (optional)
+ */
+export declare function year(date?: Date): number;
+/**
+ * Returns the year of the given date in bn language in string format
+ * @param date given date object (optional)
+ */
+export declare function yearInBangla(date?: Date): string;
+/**
+ * Returns the month
+ * @param date Given Date
+ */
+export declare function month(date?: Date): number;
+/**
+ * Returns month in bangla
+ * @param date
+ */
+export declare function monthInBangla(date?: Date): string;
+/**
+ * Returns month in bangla
+ * @param date
+ */
+export declare function monthInBanglaString(date?: Date): string;
+/**
+ * Returns month in bangla
+ * @param date
+ */
+export declare function day(date?: Date): number;
+export declare function dayInBangla(date?: Date): string;
+export declare function format(dateFormat?: string, date?: Date): string;
